7b035e0f0048c0333f97c33eb32a77004587b85d
MINZMANN <hinzmann1993@yahoo.de>
2014-10-09 15:16:39 +0200

    Add car-data to sessionstorage.

--- login.php
+++ login.php
@@ -10,22 +10,42 @@ if(!$db)
   exit("Verbindungsfehler: ".mysqli_connect_error());
 }
 
-$sqlQuery = "SELECT * FROM Benutzer WHERE LoginName = '".$loginName."' AND LoginPW = '".$loginPw."'";
+$sqlQuery = "SELECT * FROM User u LEFT JOIN Cars c ON CarID = c.ID WHERE LoginName =  '".$loginName."' AND LoginPW =  '".$loginPw."'";
+
+//$sqlQuery = "SELECT * FROM User WHERE LoginName =  '".$loginName."' AND LoginPW =  '".$loginPw."'";
+
 $result = mysqli_query($db, $sqlQuery);
 $rows = $result->num_rows;
 $successful = ($rows == 1);
 
 if($successful) {
-	$resultData = $result->fetch_assoc();
+	$resultData = $result->fetch_array();
+	
+	/*
+	$car = array {
+		'id'				=> $resultData['CarID'],
+		'modelName'			=> $resultData['ModelName'],
+		'licensePlate'		=> $resultData['LicensePlate'],
+		'seats'				=> $resultData['Seats'],
+		'constructionYear'	=> $resultData['ConstructionYear'],
+		'picID'				=> $resultData['PicID']
+	};
+	*/
 	
 	$loginResult = array(
         'successful'    => $successful,
-		'id'			=> $resultData['ID'],
-        'name'          => $resultData['Name'],
-        'email'			=> $resultData['Email'],
-        'tel'			=> $resultData['Tel'],      
-        'picID'			=> $resultData['PicID'],
-		'carID'			=> $resultData['CarID']
+		'id'			=> $resultData[0],//ID
+        'name'          => $resultData[3],//Name
+        'email'			=> $resultData[4],//Email
+        'tel'			=> $resultData[5],//Tel
+        'picID'			=> $resultData[6],//PicID
+		'carID'			=> $resultData[7],//CarID
+		'modelName'			=> $resultData[9],//ModelName
+		'licensePlate'		=> $resultData[10],//LicensePlate
+		'seats'				=> $resultData[11],//Seats
+		'constructionYear'	=> $resultData[12],//ConstructionYear
+		'carPicID'				=> $resultData[13]//PicID
+	//	'car'			=> $car,
 	);
 	exit(json_encode($loginResult));
 } else {
