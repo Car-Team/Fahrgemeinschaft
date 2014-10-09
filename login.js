7af8c0f18de8200db0d5426ae998423e727f26b8
FreshGer <SBittner@gmx.net>
2014-10-10 00:19:10 +0200

    all heads got needed .js's! + minor fixes overall

--- login.js
+++ login.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
 			dataType: "json",
 			success:	function(loginResult) {
 							if(loginResult.successful){
-								sessionStorage.setItem("userdata", JSON.stringify({
+								localStorage.setItem("userdata", JSON.stringify({
 									id: loginResult.id, 
 									name: loginResult.name, 
 									email: loginResult.email, 
@@ -39,3 +39,47 @@ $(document).ready(function() {
 		});
 	});
 });
+
+//to be able to login again!
+/*(document).on('pagebeforeshow', '#index', function(){ 
+		$("#login").click(function(){
+			alert("jo")
+		var loginName = $('#nameInput').val();
+		var loginPw = $('#pwInput').val();
+			
+		var loginData = {
+			'dbuser' : 'car',
+			'dbpw' : 'car',
+			'loginName' : loginName,
+			'loginPw' : loginPw
+		}
+
+		if(loginName == '' || loginPw == '')
+			return;
+		
+		$.ajax({
+			type: "POST",
+			url: "login.php",
+			data: loginData,
+			dataType: "json",
+			success:	function(loginResult) {
+							if(loginResult.successful){
+								localStorage.setItem("userdata", JSON.stringify({
+									id: loginResult.id, 
+									name: loginResult.name, 
+									email: loginResult.email, 
+									tel: loginResult.tel, 
+									picid: loginResult.picID, 
+									carid: loginResult.carID,
+									modelName: loginResult.modelName,
+									licensePlate: loginResult.licensePlate,
+									seats: loginResult.seats,
+									constructionYear: loginResult.constructionYear}));
+								$.mobile.changePage("menu.html");
+							}else {
+								alert("Falscher Benutzername oder falsches Passwort!");
+							}
+						},
+		});
+	});
+});*/
