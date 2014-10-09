5855c1c07f6e013754b9ae160a1a94bedae9c30c
FreshGer <SBittner@gmx.net>
2014-10-09 12:35:06 +0200

    Autoresize image acording to the screensize
    resize

--- car.js
+++ car.js
@@ -2,6 +2,27 @@
 	//$(document).on('page:load', ready)
 		//alert("Hello world!");
 		//alert(bodywidth);//alert(document.getElementById("page").offsetWidth)
+
+		
+		alert($(window).width())
+		if($(window).width()<450){
+			 $("#profile_block_a").css({ // resize the image     			
+	     			'width': 'calc(98%)'
+			   	});
+			   	$("#profile_block_b").css({ // resize the image     			
+	     			'width': 'calc(100%)'
+			   	});
+		}
+		 else {
+			   $("#profile_block_a").css({ // resize the image     			
+	     			'width': 'calc(38%)'
+			   	});
+			   $("#profile_block_b").css({ // resize the image     			
+	     			'width': 'calc(60%)'
+			   	});
+			}
+
+
 		$("#profile_picture").on("click", function() {
 			var bodywidth = document.getElementById("description").offsetWidth
 			//alert('body '+ (bodywidth/100)*70 +' -> img '+ document.getElementById("profile_block_a").offsetWidth)	
@@ -48,6 +69,25 @@
 
 
 $(document).on('pagebeforeshow', '#car', function(){ 
+
+		alert($(window).width())
+		if($(window).width()<450){
+
+			$("#car_block_a").css({ // resize the image     			
+	     			'width': 'calc(98%)'
+			   	});
+			   	$("#car_block_b").css({ // resize the image     			
+	     			'width': 'calc(100%)'
+			   	});
+		} else {
+			   $("#car_block_a").css({ // resize the image     			
+	     			'width': 'calc(38%)'
+			   	});
+			   $("#car_block_b").css({ // resize the image     			
+	     			'width': 'calc(60%)'
+			   	});
+		}
+
 		$("#car_picture").on("click", function() {
 			var bodywidth = document.getElementById("description").offsetWidth
 			//alert('body '+ (bodywidth/100)*70 +' -> img '+ document.getElementById("car_block_a").offsetWidth);				
