4edbcc5a823324ef997b0149a01bcb06354d511d
FreshGer <SBittner@gmx.net>
2014-10-09 15:23:35 +0200

    Cars >prepared< for DB!

--- login.js
+++ login.js
@@ -20,7 +20,14 @@ $(document).ready(function() {
 			dataType: "json",
 			success:	function(loginResult) {
 							if(loginResult.successful){
-								sessionStorage.setItem("userdata", JSON.stringify({id: loginResult.id, name: loginResult.name, email: loginResult.email, tel: loginResult.tel, picid: loginResult.picID, carid: loginResult.carID}));
+								sessionStorage.setItem("userdata", JSON.stringify({
+									id: loginResult.id, 
+									name: loginResult.name, 
+									email: loginResult.email, 
+									tel: loginResult.tel, 
+									picid: loginResult.picID, 
+									carid: loginResult.carID,
+									modelName: loginResult.modelName}));
 								$.mobile.changePage("menu.html");
 							}else {
 								alert("Falscher Benutzername oder falsches Passwort!");
