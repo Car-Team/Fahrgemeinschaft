3cd4b4ac2d8ea2d017d919330df55188fbb0ebd8
MINZMANN <hinzmann1993@yahoo.de>
2014-10-10 13:15:02 +0200

    Fix login.js.

--- login.js
+++ login.js
@@ -10,31 +10,31 @@ function login() {
 	if(loginName == '' || loginPw == '')
 	return;
 		
-		$.ajax({
-			type: "POST",
-			url: "login.php",
-			data: loginData,
-			dataType: "json",
-			success:	function(loginResult) {
-							if(loginResult.successful){
-								localStorage.setItem("userdata", JSON.stringify({
-									id: loginResult.id, 
-									loginname: loginResult.loginname,
-									name: loginResult.name, 
-									email: loginResult.email, 
-									tel: loginResult.tel, 
-									picid: loginResult.picID, 
-									carid: loginResult.carID,
-									descriptionUser: loginResult.descriptionUser,
-									modelName: loginResult.modelName,
-									licensePlate: loginResult.licensePlate,
-									seats: loginResult.seats,
-									constructionYear: loginResult.constructionYear,
-									descriptionCar: loginResult.descriptionCar}));
-								window.document.location.href = "menu.html";
-							}else {
-								alert("Falscher Benutzername oder falsches Passwort!");
-							}
-						},
-		});
-});
+	$.ajax({
+		type: "POST",
+		url: "login.php",
+		data: loginData,
+		dataType: "json",
+		success:	function(loginResult) {
+						if(loginResult.successful){
+							localStorage.setItem("userdata", JSON.stringify({
+								id: loginResult.id, 
+								loginname: loginResult.loginname,
+								name: loginResult.name, 
+								email: loginResult.email, 
+								tel: loginResult.tel, 
+								picid: loginResult.picID, 
+								carid: loginResult.carID,
+								descriptionUser: loginResult.descriptionUser,
+								modelName: loginResult.modelName,
+								licensePlate: loginResult.licensePlate,
+								seats: loginResult.seats,
+								constructionYear: loginResult.constructionYear,
+								descriptionCar: loginResult.descriptionCar}));
+							window.document.location.href = "menu.html";
+						}else {
+							alert("Falscher Benutzername oder falsches Passwort!");
+						}
+					},
+	});
+}
