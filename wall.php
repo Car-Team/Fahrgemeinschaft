59589291a2b586ca2ecbe226aa52bde84650c188
FreshGer <SBittner@gmx.net>
2014-10-11 23:43:04 +0200

    fixes in db communication

--- wall.php
+++ wall.php
@@ -3,6 +3,7 @@
 	$loginName = $_POST['loginName'];
 
 	$db = mysqli_connect("87.230.14.183", "car", "car", "car");
+	mysqli_query($db, "SET NAMES 'utf8'");
 	$sqlQuery = "SELECT * FROM Walls WHERE ReceiverID = '$loginID' ORDER BY Timestamp DESC";
 	$result = mysqli_query($db, $sqlQuery) or die (mysql_error());
 	//echo "<ul>";//while ($row = mysqli_fetch_array($result))//{//	echo "<li>";//    echo $row['ID'] . " " . $row['Sender'];//    echo "</li>";//}//echo "</ul>";
