7b769e86037be0e00cbfec15f8c9ad3c72a62068
MaikGroneberg <maik_grone@gmx.de>
2014-10-23 21:07:05 +0200

    fix logout -.- fuck href auf index
    lasst den scheiß !!

--- script/facebookAPI.js
+++ script/facebookAPI.js
@@ -3,6 +3,23 @@ $(document).ready(function() {
   //if(JSON.parse(localStorage.getItem('userdata')) != undefined) {
   //  $.mobile.changePage("menu.html");
   //}
+  // checkConnection();
+
+  // function checkConnection() {
+  //           var networkState = navigator.connection.type;
+
+  //           var states = {};
+  //           states[Connection.UNKNOWN]  = 'Unknown connection';
+  //           states[Connection.ETHERNET] = 'Ethernet connection';
+  //           states[Connection.WIFI]     = 'WiFi connection';
+  //           states[Connection.CELL_2G]  = 'Cell 2G connection';
+  //           states[Connection.CELL_3G]  = 'Cell 3G connection';
+  //           states[Connection.CELL_4G]  = 'Cell 4G connection';
+  //           states[Connection.CELL]     = 'Cell generic connection';
+  //           states[Connection.NONE]     = 'No network connection';
+
+  //           console.log('Connection type: ' + states[networkState]);
+  // }
 
   // Load the SDK asynchronously
   (function(d, s, id) {
