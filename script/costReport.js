6356891858b3e629fe1adecdda1ffa854bc5f219
MHinzmann <hinzmann1993@yahoo.de>
2014-11-11 18:26:11 +0100

    Improve costReport.

--- script/costReport.js
+++ script/costReport.js
@@ -1,39 +1,72 @@
+//@author MHinzmann
+
 $(document).on("pagebeforeshow", "#costReport", function() {
+	clearCollapsibles();
 	fillCollapsibles();
 });
 
-function fillCollapsibles() {
-	fillValues();
-	fillPersons();
+function clearCollapsibles() {
+	$('#deptors').empty();
+	$('#creditors').empty();
 }
 
-function fillValues() {
-	var liabilities = -685.36;
-	var receivables = 2022.78;
+function fillCollapsibles() {
+	$id = JSON.parse(localStorage.getItem("userdata")).id;
+	var data = {
+		'id' : $id,
+	}
+	
+	$.ajax({
+		type: "GET",
+		// url: "http://www.carteam.lvps87-230-14-183.dedicated.hosteurope.de/costReport.php",
+		url: "php/costReport.php",
+		data: data,
+		dataType: "jsonp",
+		success:	function(persons) {
+			fillPage(persons.debtors, persons.creditors);
+		}
+	});
+}
+
+function fillPage(debtors, creditors) {
+	var liabilities = 0;
+	var receivables = 0;
+	
+	debtors.forEach(function(person) {
+		receivables += person.debt;
+		appendPerson($('#deptors'), person);
+	});
+	
+	creditors.forEach(function(person) {
+		liabilities += person.debt;
+		appendPerson($('#creditors'), person);
+	});
+	
 	var saldo = receivables + liabilities;
 	
-	$("#liabilities").html(getColoredValue(liabilities));
-	$("#receivables").html(getColoredValue(receivables));
-	$("#saldo").html(getColoredValue(saldo));
+	$("#liabilities").html(calcColoredHtml(liabilities));
+	$("#receivables").html(calcColoredHtml(receivables));
+	$("#saldo").html(calcColoredHtml(saldo));
 }
 
-function fillPersons() {
-	//appendPerson($('#creditors'), "Karl", "media/img/avatar.jpg", -685.36);
-}
-
-function appendPerson(to, name, imgSrc, value) {
+function appendPerson(to, person) {
 	var li = document.createElement("li");
 	{
 		var a = document.createElement("a");
 		{
+			$(a).click(function() {
+				localStorage.setItem("costReportUser", JSON.stringify(person));
+				$.mobile.changePage("costReportUser.html");
+			});
+			
 			var img = document.createElement("img");
-			$(img).attr("src", imgSrc);
+			$(img).attr("src", person.picid);
 		
 			var span1 = document.createElement("span");
-			$(span1).html(name);
+			$(span1).html(person.name);
 			var span2 = document.createElement("span");
 			$(span2).attr("style", "float:right");
-			$(span2).html(getColoredValue(value));
+			$(span2).html(calcColoredHtml(person.debt));
 		
 			a.appendChild(img);
 			a.appendChild(span1);
@@ -46,14 +79,16 @@ function appendPerson(to, name, imgSrc, value) {
 	$(to).listview("refresh");
 }
 
-function getColoredValue(value) {
-	var color	= "black";
+function calcColoredHtml(value) {
+	if(value < 0.0)
+		return getColoredHtml(value, "red");
+	else if(value > 0.0)
+		return getColoredHtml(value, "green");
+	else
+		return getColoredHtml(value, "black");
+}
 
-	if(value > 0)
-		color = "green";
-	else if(value < 0)
-		color = "red";
-
+function getColoredHtml(value, color) {
 	return "<font color='" + color + "'>" + value + "</font> Euro";
 }
 
