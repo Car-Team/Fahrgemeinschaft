3bd3c7124e75a4eef345fc518f350eb6cb9fa63f
FreshGer <SBittner@gmx.net>
2014-11-12 14:14:01 +0100

    "CorpateDesign" Layout ShowARide

--- script/showARide.js
+++ script/showARide.js
@@ -6,7 +6,7 @@ viewRideID = 9;
 var viewRideData = {
 			'viewRideID' : viewRideID
 		}
-		
+
 $.ajax({
 		type: "POST",
 		url: "php/showARide.php",	
@@ -16,6 +16,16 @@ $.ajax({
 			
 			alert("result data der ID: " + viewRideResult.ID);
 //`ID`,`group`,`driver_id`,`price`,`date`,`departure_time`,`departure`,`destination`,`free_places`,`car_name`,`ride_infos` 
+			/*document.getElementById("date").innerHTML = 			viewRideResult.date;
+			document.getElementById("departureTime").innerHTML = 	viewRideResult.departure_time;
+			document.getElementById("departure").innerHTML = 		viewRideResult.departure;
+			document.getElementById("destination").innerHTML = 		viewRideResult.destination;
+			document.getElementById("freePlaces").innerHTML = 		viewRideResult.free_places;
+			document.getElementById("price").innerHTML = 			viewRideResult.price;
+			document.getElementById("carName").innerHTML = 			viewRideResult.car_name;
+			document.getElementById("info").innerHTML = 			viewRideResult.ride_infos;
+			document.getElementById("driverName").innerHTML = 		viewRideResult.name;*/
+
 			document.getElementById("date").value = 			viewRideResult.date;
 			document.getElementById("departureTime").value = 	viewRideResult.departure_time;
 			document.getElementById("departure").value = 		viewRideResult.departure;
